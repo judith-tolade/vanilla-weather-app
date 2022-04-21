0073eb6fbe6fba10c7f2ad4b3c4b4ca71d8d37d3
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-04-21 23:36:01 +0100

    update current date and time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
+function formatDate(timestamp){
+    let date =new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+        }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature (response) {
     let temperatureSelector= document.querySelector("#temperature");
     let citySelector =document.querySelector("#city");
     let descriptionSelector = document.querySelector("#description");
     let humiditySelector = document.querySelector("#humidity");
-    let windSelector = document.querySelector("#wind")
+    let windSelector = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureSelector.innerHTML= Math.round(response.data.main.temp);
     citySelector.innerHTML= response.data.name;
     descriptionSelector.innerHTML= response.data.weather[0].description;
     humiditySelector.innerHTML = response.data.main.humidity;
     windSelector.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c0efafb3b4678b54c983a9d3289a0c0a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Ibadan&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+ axios.get(apiUrl).then(displayTemperature);
