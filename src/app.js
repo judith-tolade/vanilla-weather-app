54e77ef29aee711ba66dde6c3c30a24ff556fbed
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-04-28 15:41:46 +0100

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,31 +21,56 @@ function formatDate(timestamp){
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
+return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Thur", "Fri", "Sat", "Sun", "Mon"] ;
-
     let forecastHTML =`<div class="row">`;
-    days.forEach(function (day) {
-        forecastHTML = forecastHTML + 
+    forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
         `
-         <div class="col">
-            <div class="forecast-date"> ${day}</div>
-              <i class="fa-solid fa-cloud"></i>
+         <div class="col-2">
+            <div class="forecast-date"> ${formatDay(forecastDay.dt)}</div>
+            
+              <img
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt=""
+              width="43"
+              />
               <div class="forecast-temperature">
-              <span class="forecast-temperature-max">36 &deg;C</span>
-              <span class="forecast-temperatusssre-min">24 &deg;C </span>
+              <span class="forecast-temperature-max">
+               ${Math.round(forecastDay.temp.max)}°</span>
+              <span class="forecast-temperatusssre-min"> 
+              ${Math.round(forecastDay.temp.min)}° </span>
               </div>
           </div>
         `;
+    }
     });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
     
 }
 
+function getForecast(coordinates){
+    let apiKey = "c0efafb3b4678b54c983a9d3289a0c0a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+    lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature (response) {
     let temperatureSelector= document.querySelector("#temperature");
@@ -68,6 +93,8 @@ function displayTemperature (response) {
         response.data.weather[0].icon + ".png"
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -113,4 +140,3 @@ fahrenheitLink.addEventListener("click", displayFahTemperature);
 
 
 search("Ibadan");
-displayForecast();
