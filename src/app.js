6dd6d75f1023bce6529f5cdb990bd9556db29c9b
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-04-29 13:59:58 +0100

    Corrected API URL for the forecast

--- src/app.js
+++ src/app.js
@@ -67,8 +67,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates){
     let apiKey = "c0efafb3b4678b54c983a9d3289a0c0a";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-    lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 
